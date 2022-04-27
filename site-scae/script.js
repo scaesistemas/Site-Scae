5b172005067013334006d20af1c917919aa0467a
Nelson-Ti <nelson-marques147@outlook.com>
2022-04-27 16:48:27 -0300

    ajustes para mobile

--- site-scae/script.js
+++ site-scae/script.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
-        loop: true,
+        loop: false,
         autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
